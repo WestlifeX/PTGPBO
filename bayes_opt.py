03a17e61cf5f44aaa0486f6c90395fd8f4470d13
WestlifeX <1165996282@qq.com>
2022-09-18 20:02:27 +0800

    update

--- bayes_opt.py
+++ bayes_opt.py
@@ -1,13 +1,14 @@
 import torch
 import sys
 
+
 class BayesOptimiser(object):
-    
+
     def __init__(self, gp, acq_func, epsilon=1e-8):
         self.gp = gp
         self.acq_func = acq_func
         self.epsilon = epsilon
-    
+
     def minimise(self, objective, bounds, iters, iter_loops=20,
                  sub_opt_iters=100, sub_opt_lr=0.05, n_inits=5, verbose=True):
         x_hist = None
@@ -16,74 +17,75 @@ class BayesOptimiser(object):
         x_min = None
         beta1 = 0.9
         beta2 = 0.99
-        
-        for x_sample in bounds[:,0].view(1,-1) + torch.rand((n_inits,1)) * \
-        (bounds[:,1] - bounds[:,0]).view(1,-1):
+
+        for x_sample in bounds[:, 0].view(1, -1) + torch.rand((n_inits, 1)) * \
+                        (bounds[:, 1] - bounds[:, 0]).view(1, -1):
             y_sample = objective(x_sample)
             if y_sample < y_min:
                 y_min = y_sample
                 x_min = x_sample
             if x_hist is not None:
-                x_hist = torch.cat((x_hist, x_sample.view(1,-1)), dim=0)
-                y_hist = torch.cat((y_hist, y_sample.view(1,-1)), dim=0)
+                x_hist = torch.cat((x_hist, x_sample.view(1, -1)), dim=0)
+                y_hist = torch.cat((y_hist, y_sample.view(1, -1)), dim=0)
             else:
-                x_hist = x_sample.view(1,-1)
-                y_hist = y_sample.view(1,-1)
-        
+                x_hist = x_sample.view(1, -1)
+                y_hist = y_sample.view(1, -1)
+
         for i in range(iters):
             self.gp.kernel.train()
             self.gp.kernel.reset_params()
             self.gp.fit(x_hist, y_hist)
             self.gp.kernel.eval()
             y_acq = float('inf')
-            
+
             with torch.no_grad():
                 for j in range(iter_loops):
-                    x0 = bounds[:,0] + torch.rand(bounds.size(0)) * (bounds[:,1] - bounds[:,0])
+                    x0 = bounds[:, 0] + torch.rand(bounds.size(0)) * (bounds[:, 1] - bounds[:, 0])
                     V_dx = torch.zeros(x0.size())
                     S_dx = torch.zeros(x0.size())
                     for k in range(sub_opt_iters):
                         dx = self.acq_func.grad(x0, y_min, self.gp).flatten()
                         V_dx = beta1 * V_dx + (1 - beta1) * dx
-                        V_dx_corr = V_dx / (1 - beta1**(k+1))
+                        V_dx_corr = V_dx / (1 - beta1 ** (k + 1))
                         S_dx = beta2 * S_dx + (1 - beta2) * dx.pow(2)
-                        S_dx_corr = S_dx / (1 - beta2**(k+1))
+                        S_dx_corr = S_dx / (1 - beta2 ** (k + 1))
                         grad = V_dx_corr / (torch.sqrt(S_dx_corr + 1e-8))
-                        
+
                         x0 = x0 - sub_opt_lr * grad
-                        x_bounded = torch.min(torch.max(x0.data, bounds[:,0]), bounds[:,1])
+                        x_bounded = torch.min(torch.max(x0.data, bounds[:, 0]), bounds[:, 1])
                         x0.data = x_bounded
                     acq = self.acq_func(x0, y_min, self.gp)
-                    if acq < y_acq: 
+                    if acq < y_acq:
                         y_acq = acq
                         x_sample = x0
-                
-                distances = (x_sample.view(1,-1) - x_hist).pow(2).sum(1).pow(0.5)
+
+                distances = (x_sample.view(1, -1) - x_hist).pow(2).sum(1).pow(0.5)
                 if torch.any(distances <= self.epsilon):
-                    x_sample = bounds[:,0] + torch.rand(bounds.size(0)) * (bounds[:,1] - bounds[:,0])
-                
+                    x_sample = bounds[:, 0] + torch.rand(bounds.size(0)) * (bounds[:, 1] - bounds[:, 0])
+
                 y_sample = objective(x_sample)
-                x_hist = torch.cat((x_hist, x_sample.view(1,-1)), dim=0)
-                y_hist = torch.cat((y_hist, y_sample.view(1,-1)), dim=0)
-                
+                x_hist = torch.cat((x_hist, x_sample.view(1, -1)), dim=0)
+                y_hist = torch.cat((y_hist, y_sample.view(1, -1)), dim=0)
+
                 if y_sample < y_min:
                     y_min = y_sample
                     x_min = x_sample
-                
+
                 if verbose:
                     sys.stdout.write('\rIteration: %d   Minimum: %4.2f   ' % \
-                                     (i+1, y_min))
+                                     (i + 1, y_min))
                     sys.stdout.flush()
-        
+
         return x_min, y_min, x_hist, y_hist
 
+
 class DynamicBayesOptimiser(object):
-    
+
     def __init__(self, gp, acq_func, epsilon=1e-8):
         self.gp = gp
         self.acq_func = acq_func
-        self.epsilon = epsilon
-    
+        self.epsilon = epsilon  # epsilon什么用
+
     def minimise(self, objective, bounds, iters, iter_loops=20,
                  sub_opt_iters=100, sub_opt_lr=0.01, n_inits=5, verbose=True):
         x_hist = None
@@ -93,24 +95,25 @@ class DynamicBayesOptimiser(object):
         x_min = None
         beta1 = 0.9
         beta2 = 0.99
-        
-        for x_sample in bounds[:,0].view(1,-1) + torch.rand((n_inits,1)) * \
-        (bounds[:,1] - bounds[:,0]).view(1,-1):
+
+        # 就是从bound的下界遍历到上界，不过上下界加了点高斯噪声
+        for x_sample in bounds[:, 0].view(1, -1) + torch.rand((n_inits, 1)) * \
+                        (bounds[:, 1] - bounds[:, 0]).view(1, -1):
             y_sample = objective(x_sample)
             if y_sample < y_min:
                 y_min = y_sample
                 x_min = x_sample
             if x_hist is not None:
-                x_hist = torch.cat((x_hist, x_sample.view(1,-1)), dim=0)
-                t_hist = torch.cat((t_hist, t_hist[-1] + torch.ones((1,1))), dim=0)
-                y_hist = torch.cat((y_hist, y_sample.view(1,-1)), dim=0)
+                x_hist = torch.cat((x_hist, x_sample.view(1, -1)), dim=0)
+                t_hist = torch.cat((t_hist, t_hist[-1] + torch.ones((1, 1))), dim=0)
+                y_hist = torch.cat((y_hist, y_sample.view(1, -1)), dim=0)
             else:
-                x_hist = x_sample.view(1,-1)
-                t_hist = torch.ones((1,1))
-                y_hist = y_sample.view(1,-1)
-        
+                x_hist = x_sample.view(1, -1)
+                t_hist = torch.ones((1, 1))
+                y_hist = y_sample.view(1, -1)
+
         for i in range(iters):
-            self.gp.space_kernel.train()
+            self.gp.space_kernel.train()  # train是哪来的，train啥
             self.gp.time_kernel.train()
             self.gp.space_kernel.reset_params()
             self.gp.time_kernel.reset_params()
@@ -118,86 +121,88 @@ class DynamicBayesOptimiser(object):
             self.gp.space_kernel.eval()
             self.gp.time_kernel.eval()
             y_acq = float('inf')
-            
+
             with torch.no_grad():
                 for j in range(iter_loops):
-                    x0 = bounds[:,0] + torch.rand(bounds.size(0)) * (bounds[:,1] - bounds[:,0])
+                    x0 = bounds[:, 0] + torch.rand(bounds.size(0)) * (bounds[:, 1] - bounds[:, 0])
                     V_dx = torch.zeros(x0.size())
                     S_dx = torch.zeros(x0.size())
                     for k in range(sub_opt_iters):
-                        dx = self.acq_func.grad(x0, t_hist[-1] + torch.ones((1,1)), y_min, self.gp).flatten()
+                        dx = self.acq_func.grad(x0, t_hist[-1] + torch.ones((1, 1)), y_min, self.gp).flatten()
                         V_dx = beta1 * V_dx + (1 - beta1) * dx
-                        V_dx_corr = V_dx / (1 - beta1**(k+1))
+                        V_dx_corr = V_dx / (1 - beta1 ** (k + 1))
                         S_dx = beta2 * S_dx + (1 - beta2) * dx.pow(2)
-                        S_dx_corr = S_dx / (1 - beta2**(k+1))
+                        S_dx_corr = S_dx / (1 - beta2 ** (k + 1))
                         grad = V_dx_corr / (torch.sqrt(S_dx_corr + 1e-8))
-                        
+
                         x0 = x0 - sub_opt_lr * grad
-                        x_bounded = torch.min(torch.max(x0.data, bounds[:,0]), bounds[:,1])
+                        x_bounded = torch.min(torch.max(x0.data, bounds[:, 0]), bounds[:, 1])
                         x0.data = x_bounded
-                    acq = self.acq_func(x0, t_hist[-1] + torch.ones((1,1)), y_min, self.gp)
-                    if acq < y_acq: 
+                    acq = self.acq_func(x0, t_hist[-1] + torch.ones((1, 1)), y_min, self.gp)
+                    if acq < y_acq:
                         y_acq = acq
                         x_sample = x0
-                
-                distances = (x_sample.view(1,-1) - x_hist).pow(2).sum(1).pow(0.5)
+
+                distances = (x_sample.view(1, -1) - x_hist).pow(2).sum(1).pow(0.5)
                 if torch.any(distances <= self.epsilon):
-                    x_sample = bounds[:,0] + torch.rand(bounds.size(0)) * (bounds[:,1] - bounds[:,0])
-                
+                    x_sample = bounds[:, 0] + torch.rand(bounds.size(0)) * (bounds[:, 1] - bounds[:, 0])
+
                 y_sample = objective(x_sample)
-                x_hist = torch.cat((x_hist, x_sample.view(1,-1)), dim=0)
-                t_hist = torch.cat((t_hist, t_hist[-1] + torch.ones((1,1))), dim=0)
-                y_hist = torch.cat((y_hist, y_sample.view(1,-1)), dim=0)
-                
+                x_hist = torch.cat((x_hist, x_sample.view(1, -1)), dim=0)
+                t_hist = torch.cat((t_hist, t_hist[-1] + torch.ones((1, 1))), dim=0)
+                y_hist = torch.cat((y_hist, y_sample.view(1, -1)), dim=0)
+
                 if y_sample < y_min:
                     y_min = y_sample
                     x_min = x_sample
-                
+
                 if verbose:
                     sys.stdout.write('\rIteration: %d   Minimum: %4.2f   ' % \
-                                     (i+1, y_min))
+                                     (i + 1, y_min))
                     sys.stdout.flush()
-        
+
         return x_min, y_min, x_hist, y_hist
 
+
 class Acquisition(object):
-    
+
     def __init__(self, beta):
         self.beta = beta
-    
+
     def __call__(self, x, y_min, gp):
         if len(x.size()) < 2:
-            x = x.view(1,-1)
+            x = x.view(1, -1)
         mu, sigma = gp.predict(x, return_var=True)
-        out = mu - self.beta*sigma
+        out = mu - self.beta * sigma
         return out
-    
+
     def grad(self, x, y_min, gp):
         if len(x.size()) < 2:
-            x = x.view(1,-1)
+            x = x.view(1, -1)
         mu_grad = gp.mu_grad(x)
         sigma_grad = gp.sigma_grad(x)
-        grad = mu_grad - self.beta*sigma_grad
+        grad = mu_grad - self.beta * sigma_grad
         return grad
 
+
 class DynamicAcquisition(object):
-    
+
     def __init__(self, beta):
         self.beta = beta
-    
+
     def __call__(self, x, t, y_min, gp):
         if len(x.size()) < 2:
-            x = x.view(1,-1)
-            t = t.view(1,-1)
+            x = x.view(1, -1)
+            t = t.view(1, -1)
         mu, sigma = gp.predict(x, t, return_var=True)
-        out = mu - self.beta*sigma
+        out = mu - self.beta * sigma
         return out
-    
+
     def grad(self, x, t, y_min, gp):
         if len(x.size()) < 2:
-            x = x.view(1,-1)
-            t = t.view(1,-1)
+            x = x.view(1, -1)
+            t = t.view(1, -1)
         mu_grad = gp.mu_grad(x, t)
         sigma_grad = gp.sigma_grad(x, t)
-        grad = mu_grad - self.beta*sigma_grad
+        grad = mu_grad - self.beta * sigma_grad
         return grad
--- gaussian_process.py
+++ gaussian_process.py
@@ -1,26 +1,32 @@
 import torch
 from copy import deepcopy
 
+
 class GaussianProcess(object):
-    
+
     def __init__(self, kernel, alpha=1e-5):
         self.kernel = kernel
         self.alpha = alpha
-    
+        # Assuming the training data or objective function is noisy,
+        # alpha should be set to the standard deviation of the noise
+
+    # nll: negative log likelihood， 干什么用的还不明确
     def nll(self, x1, x2, y, det_tol=1e-12):
-        b = y.size(0)
-        m = y.size(1)
-        k = self.kernel(x1, x2) + self.alpha*torch.eye(b)
-        nll = 0.5*torch.log(torch.det(k) + torch.tensor(det_tol)) + \
-        0.5*y.view(m,b) @ torch.inverse(k) @ y.view(b,m)
+        b = y.size(0)  # n
+        m = y.size(1)  # 1
+        k = self.kernel(x1, x2) + self.alpha * torch.eye(b)
+        nll = 0.5 * torch.log(torch.det(k) + torch.tensor(det_tol)) + \
+              0.5 * y.view(m, b) @ torch.inverse(k) @ y.view(b, m)
         return nll
-    
+
+    # x: nxp, y: nx1
     def fit(self, x, y, lr=0.01, iters=100, restarts=0):
-        b = x.size(0)
+        b = x.size(0)  # n
         n = x.size(1)
         x1 = x.unsqueeze(1).expand(b, b, n)
         x2 = x.unsqueeze(0).expand(b, b, n)
         optimiser = torch.optim.Adam(self.kernel.parameters(), lr)
+        assert iters > 0
         for i in range(iters):
             optimiser.zero_grad()
             nll = self.nll(x1, x2, y)
@@ -39,44 +45,44 @@ class GaussianProcess(object):
             if nll.item() < best_nll:
                 best_nll = nll
                 params = self.kernel.get_params()
-        
+
         self.kernel.set_params(params)
         self.x = x
         self.y = y
-        k = self.kernel(x1, x2).view(b,b) + self.alpha*torch.eye(b)
+        k = self.kernel(x1, x2).view(b, b) + self.alpha * torch.eye(b)
         self.kinv = torch.inverse(k)
-    
+
     def predict(self, x, return_var=False):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
         k = self.kernel(x1, x2).view(b_prime, b)
         mu = k @ self.kinv @ self.y
-        x = x.unsqueeze(1).expand(b_prime,1,n)
+        x = x.unsqueeze(1).expand(b_prime, 1, n)
         sigma = self.kernel(x, x) - (k @ self.kinv @ k.t()).diag().view(mu.size())
         if return_var:
             return mu, sigma
         else:
             return mu
-    
+
     def mu_grad(self, x):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
         dk = self.kernel.grad(x1, x2)
         grad = (dk * (self.kinv @ self.y)).sum(1)
         return grad
-    
+
     def sigma_grad(self, x):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
         x = x.unsqueeze(1)
         k = self.kernel(x1, x2)
         dk = self.kernel.grad(x1, x2)
@@ -84,22 +90,28 @@ class GaussianProcess(object):
         grad = (self.kernel.grad(x, x) - k_grad * dk).sum(1)
         return grad
 
+
 class SpatioTemporalGaussianProcess(object):
-    
+
     def __init__(self, space_kernel, time_kernel, alpha=1e-5):
         self.space_kernel = space_kernel
         self.time_kernel = time_kernel
         self.alpha = alpha
-    
+        # Assuming the training data or objective function is noisy,
+        # alpha should be set to the standard deviation of the noise
+        # alpha就是超参数σ^2
+
+    # nll: negative log likelihood， 干什么用的还不明确
     def nll(self, x1, x2, t1, t2, y, det_tol=1e-12):
         b = y.size(0)
         m = y.size(1)
         k = self.space_kernel(x1, x2) * self.time_kernel(t1, t2) + \
-        self.alpha*torch.eye(b)
-        nll = 0.5*torch.log(torch.det(k) + torch.tensor(det_tol)) + \
-        0.5*y.view(m,b) @ torch.inverse(k) @ y.view(b,m)
+            self.alpha * torch.eye(b)
+        nll = 0.5 * torch.log(torch.det(k) + torch.tensor(det_tol)) + \
+              0.5 * y.view(m, b) @ torch.inverse(k) @ y.view(b, m)
         return nll
-    
+
+    # 反正就是用nll作为loss来算k逆
     def fit(self, x, t, y, lr=0.01, iters=100, restarts=0):
         b = x.size(0)
         n = x.size(1)
@@ -117,6 +129,7 @@ class SpatioTemporalGaussianProcess(object):
         best_nll = nll.item()
         space_params = self.space_kernel.get_params()
         time_params = self.time_kernel.get_params()
+        # 为什么还有个restarts
         for i in range(restarts):
             self.kernel.randomise_params()
             optimiser = torch.optim.Adam(list(self.space_kernel.parameters()) + \
@@ -130,57 +143,58 @@ class SpatioTemporalGaussianProcess(object):
                 best_nll = nll
                 space_params = self.space_kernel.get_params()
                 time_params = self.time_kernel.get_params()
-        
+
         self.space_kernel.set_params(space_params)
         self.time_kernel.set_params(time_params)
         self.x = x
         self.t = t
         self.y = y
         k = self.space_kernel(x1, x2) * self.time_kernel(t1, t2) + \
-        self.alpha*torch.eye(b)
+            self.alpha * torch.eye(b)
         self.kinv = torch.inverse(k)
-    
+
+    # predict没啥问题，按照公式来
     def predict(self, x, t, return_var=False):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
-        t1 = t.unsqueeze(1).expand(b_prime,b,1)
-        t2 = self.t.unsqueeze(0).expand(b_prime,b,1)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
+        t1 = t.unsqueeze(1).expand(b_prime, b, 1)
+        t2 = self.t.unsqueeze(0).expand(b_prime, b, 1)
         k = self.space_kernel(x1, x2) * self.time_kernel(t1, t2)
         mu = k @ self.kinv @ self.y
-        x = x.unsqueeze(1).expand(b_prime,1,n)
-        t = t.unsqueeze(1).expand(b_prime,1,1)
+        x = x.unsqueeze(1).expand(b_prime, 1, n)
+        t = t.unsqueeze(1).expand(b_prime, 1, 1)
         sigma = self.space_kernel(x, x) * self.time_kernel(t, t) - \
-        (k @ self.kinv @ k.t()).diag().view(mu.size())
+                (k @ self.kinv @ k.t()).diag().view(mu.size())
         if return_var:
             return mu, sigma
         else:
             return mu
-    
+
     def mu_grad(self, x, t):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
-        t1 = t.unsqueeze(1).expand(b_prime,b,1)
-        t2 = self.t.unsqueeze(0).expand(b_prime,b,1)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
+        t1 = t.unsqueeze(1).expand(b_prime, b, 1)
+        t2 = self.t.unsqueeze(0).expand(b_prime, b, 1)
         dk = self.space_kernel.grad(x1, x2) * self.time_kernel(t1, t2).unsqueeze(-1)
         grad = (dk * (self.kinv @ self.y)).sum(1)
         return grad
-    
+
     def sigma_grad(self, x, t):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
-        t1 = t.unsqueeze(1).expand(b_prime,b,1)
-        t2 = self.t.unsqueeze(0).expand(b_prime,b,1)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
+        t1 = t.unsqueeze(1).expand(b_prime, b, 1)
+        t2 = self.t.unsqueeze(0).expand(b_prime, b, 1)
         x = x.unsqueeze(1)
-        t = t.unsqueeze(1).expand(b_prime,1,1)
+        t = t.unsqueeze(1).expand(b_prime, 1, 1)
         k = self.space_kernel(x1, x2) * self.time_kernel(t1, t2)
         dk = self.space_kernel.grad(x1, x2) * self.time_kernel(t1, t2).unsqueeze(-1)
         k_grad = (k @ self.kinv + (self.kinv @ k.t()).t()).unsqueeze(-1)
@@ -188,31 +202,32 @@ class SpatioTemporalGaussianProcess(object):
                 self.time_kernel(t, t).unsqueeze(-1) - k_grad * dk).sum(1)
         return grad
 
+
 class AutomaticGaussianProcess(object):
-    
+
     def __init__(self, kernel_list, init_kernel=None, alpha=1e-5):
         self.kernel_list = kernel_list
         self.kernel = init_kernel
         self.alpha = alpha
-    
+
     def nll(self, x1, x2, y, det_tol=1e-12):
         b = y.size(0)
         m = y.size(1)
-        k = self.kernel(x1, x2) + self.alpha*torch.eye(b)
-        nll = 0.5*torch.log(torch.det(k) + torch.tensor(det_tol)) + \
-        0.5*y.view(m,b) @ torch.inverse(k) @ y.view(b,m)
+        k = self.kernel(x1, x2) + self.alpha * torch.eye(b)
+        nll = 0.5 * torch.log(torch.det(k) + torch.tensor(det_tol)) + \
+              0.5 * y.view(m, b) @ torch.inverse(k) @ y.view(b, m)
         return nll
-    
+
     def fit(self, x, y, kernel_search_iters=3, lr=0.01, iters=100, restarts=0,
             verbose=False):
         b = x.size(0)
         n = x.size(1)
         x1 = x.unsqueeze(1).expand(b, b, n)
         x2 = x.unsqueeze(0).expand(b, b, n)
-        
+
         previous_kernel_nll = None
         no_improvement = False
-        
+
         for s in range(kernel_search_iters):
             if no_improvement:
                 break
@@ -247,7 +262,7 @@ class AutomaticGaussianProcess(object):
                 self.kernel = best_kernel
                 previous_kernel_nll = best_kernel_nll
                 if verbose:
-                    print('\nsearch iter: %d, best nll: %f' % (s+1, best_kernel_nll))
+                    print('\nsearch iter: %d, best nll: %f' % (s + 1, best_kernel_nll))
                     print(self.kernel)
             else:
                 if previous_kernel_nll is None:
@@ -279,10 +294,10 @@ class AutomaticGaussianProcess(object):
                     previous_kernel = deepcopy(self.kernel)
                     best_kernel_nll = previous_kernel_nll
                     best_kernel = previous_kernel
-                
+
                 for k in range(len(self.kernel_list)):
                     self.kernel = deepcopy(previous_kernel) + \
-                    deepcopy(self.kernel_list[k])
+                                  deepcopy(self.kernel_list[k])
                     self.kernel.reset_params()
                     optimiser = torch.optim.Adam(self.kernel.parameters(), lr)
                     for i in range(iters):
@@ -307,9 +322,9 @@ class AutomaticGaussianProcess(object):
                         if best_nll < best_kernel_nll:
                             best_kernel_nll = best_nll
                             best_kernel = deepcopy(self.kernel)
-                    
+
                     self.kernel = deepcopy(previous_kernel) * \
-                    deepcopy(self.kernel_list[k])
+                                  deepcopy(self.kernel_list[k])
                     optimiser = torch.optim.Adam(self.kernel.parameters(), lr)
                     for i in range(iters):
                         optimiser.zero_grad()
@@ -338,45 +353,45 @@ class AutomaticGaussianProcess(object):
                 self.kernel = best_kernel
                 previous_kernel_nll = best_kernel_nll
                 if verbose:
-                    print('\nsearch iter: %d, best nll: %f' % (s+1, best_kernel_nll))
+                    print('\nsearch iter: %d, best nll: %f' % (s + 1, best_kernel_nll))
                     print(self.kernel)
-        
+
         self.x = x
         self.y = y
-        k = self.kernel(x1, x2).view(b,b) + self.alpha*torch.eye(b)
+        k = self.kernel(x1, x2).view(b, b) + self.alpha * torch.eye(b)
         self.kinv = torch.inverse(k)
-    
+
     def predict(self, x, return_var=False):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
         k = self.kernel(x1, x2).view(b_prime, b)
         mu = k @ self.kinv @ self.y
-        x = x.unsqueeze(1).expand(b_prime,1,n)
+        x = x.unsqueeze(1).expand(b_prime, 1, n)
         sigma = self.kernel(x, x) - (k @ self.kinv @ k.t()).diag().view(mu.size())
         if return_var:
             return mu, sigma
         else:
             return mu
-    
+
     def mu_grad(self, x):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
         dk = self.kernel.grad(x1, x2)
         grad = (dk * (self.kinv @ self.y)).sum(1)
         return grad
-    
+
     def sigma_grad(self, x):
         b_prime = x.size(0)
         b = self.x.size(0)
         n = self.x.size(1)
-        x1 = x.unsqueeze(1).expand(b_prime,b,n)
-        x2 = self.x.unsqueeze(0).expand(b_prime,b,n)
+        x1 = x.unsqueeze(1).expand(b_prime, b, n)
+        x2 = self.x.unsqueeze(0).expand(b_prime, b, n)
         x = x.unsqueeze(1)
         k = self.kernel(x1, x2)
         dk = self.kernel.grad(x1, x2)
